2bc459971554e84d1490cc9bfaf1f3bfb1d6457c
Adakos <59307092+bociksamocik12@users.noreply.github.com>
2022-06-23 05:43:21 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "OTg5Mjk4MzYzMzUyNjE2OTgw.GI-mNG.CSyIE_UBUVNLEOYX1PjGSHESPwYzj9U8ZTj4Lo",
+    token: "OTg5Mjk4MzYzMzUyNjE2OTgw.GJfcAz.ZNEGN4K9FwBGsl3YJEML2t0ctc-q1PXDn1d2yE",
     ticketChannel: "989355635453874206",
     ticketRoles: ["831235907666575471"],
     ticketCategory: "943618487253872670",
